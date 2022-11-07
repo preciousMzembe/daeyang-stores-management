c7444500af1f479d84971b6b877c1e308f6a8e00
Precious Mzembe <preciousmzembe@gmail.com>
2022-11-07 17:50:11 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DSMS</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="icon" type="image/png" href="files/icons/logo.png" />
+</head>
+
+<body>
+    <div class="login">
+        <div class="inner_login">
+            <div class="image"><img src="files/icons/logo.png" alt=""></div>
+            <div class="title">Daeyang Stores Management System</div>
+
+            <form class="login_form">
+                <div class="login_title">Login</div>
+
+                <div class="input_label">Username</div>
+                <div class="username_input input"><input type="text" name="username" id="username" placeholder="Username"></div>
+
+                <div class="input_label">Password</div>
+                <div class="password_input input"><input type="password" name="password" id="password" placeholder="Password"></div>
+
+                <div class="login_button"><input type="submit" value="Login"></div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
